a9f854e7a5057543b717150f481cf7ad726cdb50
Ky0sann <mathis.huard@supdevinci-edu.fr>
2024-06-02 16:34:57 +0200

    Ajout du modèle
    Ajout du modèle de la voiture en utilisant la clé étrangère id_modele en référence à id_modele de la table modele, permettant alors de récupérer les attributs de la table modele comme "libelle" (pour afficher le nom du modèle)

--- vehicle.php
+++ vehicle.php
@@ -10,12 +10,6 @@ $immatriculation = $_GET["immatriculation"];
 try { 
 $connexion = new PDO('mysql:host=localhost;dbname=locauto',
 'root', '');
-// Exercice 2 //  
-// $requete = 'SELECT id_voiture FROM voiture WHERE dept ='.$dept;
-// $resultat = $connexion->query($requete);
-// $ligne = $resultat->fetch();
-// echo "<h1> Liste des employés du département : " .$ligne["nom"]. "</h1>";
-// Suite du code pour récupérer la liste des employeurs correspondant au département 
 $requete = 'SELECT * FROM voiture WHERE immatriculation = :immatriculation';
 $stmt = $connexion->prepare($requete);
 $stmt->bindParam(':immatriculation', $immatriculation, PDO::PARAM_STR);
@@ -23,14 +17,26 @@ $stmt->execute();
 echo "<table>\n";
 echo "\t<tr><th>id_voiture</th>
 <th>immatriculation</th>
-<th>compteur</th></tr\n>";
-while ($ligne = $stmt->fetch(PDO::FETCH_ASSOC)) {
-echo "\t<tr>\n";
-echo "\t\t<td>" . $ligne["id_voiture"] . "</td>\n";
-// echo "\t\t<td><a href=formemploye.php?num=".$ligne["num"]." >" . $ligne["nom"] . "</a></td>\n";
-echo "\t\t<td>" . $ligne["immatriculation"] . "</td>\n";
-echo "\t\t<td>" . $ligne["compteur"] . "</td>\n";
-echo "\t</tr>\n";
+<th>compteur</th>
+<th>libelle</th></tr\n>";
+while ($voiture = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    // Récupération de la clé étrangère id_modele de la voiture
+    $id_modele = $voiture["id_modele"];
+    
+    // Requête pour récupérer les informations du modèle
+    $requete2 = 'SELECT * FROM modele WHERE id_modele = :id_modele';
+    $stmt2 = $connexion->prepare($requete2);
+    $stmt2->bindParam(':id_modele', $id_modele, PDO::PARAM_INT);
+    $stmt2->execute();
+    
+    $modele = $stmt2->fetch(PDO::FETCH_ASSOC);
+    
+    echo "\t<tr>\n";
+    echo "\t\t<td>" . $voiture["id_voiture"] . "</td>\n";
+    echo "\t\t<td>" . $voiture["immatriculation"] . "</td>\n";
+    echo "\t\t<td>" . $voiture["compteur"] . "</td>\n";
+    echo "\t\t<td>" . $modele["libelle"] . "</td>\n";
+    echo "\t</tr>\n";
 }
 echo "</table>";
 } catch (PDOException $e) {
